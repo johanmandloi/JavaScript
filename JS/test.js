373f30b104cfcd091e051c75c72f7fb25fac6ba2
johanmandloi <johanmandloi19@gmail.com>
2024-08-07 15:59:32 +0000

    new

---
+++ JS/test.js
@@ -0,0 +1,15 @@
+const value = 100
+let setValue = "akshat"
+let city
+
+// console.table([value, setValue, city])
+
+//  console.log("2" + 1 + 1);
+// console.log("2" + "2");
+// console.log(1 + 2 + "1" + 1 + 3);
+
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+console.log(typeof city);
